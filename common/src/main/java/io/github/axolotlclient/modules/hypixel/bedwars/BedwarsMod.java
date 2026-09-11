cd9f6149a4b1e527f710960dfda67bc1d4328e37
chitoolcese-lab <chitoolcese@gmail.com>
2026-09-10 19:19:54 -0500

    Update BedwarsMod.java

--- common/src/main/java/io/github/axolotlclient/modules/hypixel/bedwars/BedwarsMod.java
+++ common/src/main/java/io/github/axolotlclient/modules/hypixel/bedwars/BedwarsMod.java
@@ -1,5 +1,37 @@
 package io.github.axolotlclient.modules.hypixel.bedwars;
 
-public class BedwarsMod {
-    // Módulo desactivado para evitar el crash en 1.21
+import io.github.axolotlclient.AxolotlClientConfig.api.options.OptionCategory;
+import io.github.axolotlclient.modules.AbstractModule;
+
+public class BedwarsMod extends AbstractModule {
+
+    private static BedwarsMod instance;
+
+    public static BedwarsMod getInstance() {
+        return instance;
+    }
+
+    public BedwarsMod() {
+        instance = this;
+    }
+
+    @Override
+    public void init() {
+        // Módulo desactivado
+    }
+
+    @Override
+    public OptionCategory getCategory() {
+        return OptionCategory.create("bedwars");
+    }
+
+    // Métodos vacíos para que no rompa la compilación
+    public boolean inGame() {
+        return false;
+    }
+
+    public Object getUpgradesOverlay() { return null; }
+    public Object getResourceOverlay() { return null; }
+    public Object getStatsOverlay() { return null; }
+    public Object getSessionStatsOverlay() { return null; }
 }
