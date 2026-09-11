82983e0ee91d0b2a63ac1dc256b9e29cf69297bb
chitoolcese-lab <chitoolcese@gmail.com>
2026-09-10 19:29:07 -0500

    Update BedwarsMod.java

--- common/src/main/java/io/github/axolotlclient/modules/hypixel/bedwars/BedwarsMod.java
+++ common/src/main/java/io/github/axolotlclient/modules/hypixel/bedwars/BedwarsMod.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 AxolotlClient contributors
+ * Copyright © 2026 AxolotlClient contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
